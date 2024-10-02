79307ed28ebbfec107567b9329f02a8869e63e9a
Dmitry Popovichev <181736635+Dmitry-Popovichev@users.noreply.github.com>
2024-10-02 14:46:37 +0000

    Bug: script not changing the version.txt file
    Adding lots of debugs to figure out why.

--- .github/workflows/bin/version_increment.sh
+++ .github/workflows/bin/version_increment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-
+echo "Hello World!"
 version=`cat /home/runner/work/SCD-OpenStack-Utils/SCD-OpenStack-Utils/OpenStack-Rabbit-Consumer/version.txt`       #reads the version.txt file and sets the version to the current version
 
 
@@ -9,14 +9,19 @@ major=$(echo $version | cut -f1 -d.)
 minor=$(echo $version | cut -f2 -d.)
 patch=$(echo $version | cut -f3 -d.)
 
+echo $major
+echo $minor
+echo $patch
 
 #increments the patch by 1
 patch=$((patch + 1))
 
+echo $patch
 
 #concatenate the version
 newversion="$major.$minor.$patch"
 
+echo $newversion
 
 #overwrites the version.txt file with new new version
 printf "$newversion" > /home/runner/work/SCD-OpenStack-Utils/SCD-OpenStack-Utils/OpenStack-Rabbit-Consumer/version.txt
