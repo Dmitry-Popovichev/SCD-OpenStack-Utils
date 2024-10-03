2c4416ee45ee3945e6f3e75dfd04bb46c22485a5
Dmitry Popovichev <181736635+Dmitry-Popovichev@users.noreply.github.com>
2024-10-03 09:02:46 +0000

    Testing: Paths and Author stuff

--- .github/workflows/bin/version_increment.sh
+++ .github/workflows/bin/version_increment.sh
@@ -1,35 +1,20 @@
 #!/usr/bin/bash
 
-echo "Hello World!"
-echo ~0
-echo "..."
-echo ~0/OpenStack-Rabbit-Consumer/version.txt
-echo "..."
-echo ~/OpenStack-Rabbit-Consumer/version.txt
-echo "Bye"
+set -euo pipefail
 
-version=`cat ~0/OpenStack-Rabbit-Consumer/version.txt` #reads the version.txt file and sets the version to the current version
+#reads the version.txt file and sets the version to the current version
+version=`cat ~0/OpenStack-Rabbit-Consumer/version.txt`
 
-# cuts the $version variable into major, minor and patch numbers removing the fullstop
+# cuts the $version variable into major, minor and patch numbers, removing the fullstop
 major=$(echo $version | cut -f1 -d.)
 minor=$(echo $version | cut -f2 -d.)
 patch=$(echo $version | cut -f3 -d.)
 
-echo $major
-echo $minor
-echo $patch
-
 #increments the patch by 1
 patch=$((patch + 1))
-
-echo $patch
 
 #concatenate the version
 newversion="$major.$minor.$patch"
 
-echo $newversion
-
 #overwrites the version.txt file with new new version
 printf "$newversion" > ~0/OpenStack-Rabbit-Consumer/version.txt
-
-cat ~0/OpenStack-Rabbit-Consumer/version.txt
